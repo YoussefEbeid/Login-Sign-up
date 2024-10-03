446971e7703b4e390a89486ae3a29cfcb9b9a560
YoussefEbeid <114070726+YoussefEbeid@users.noreply.github.com>
2024-10-03 10:21:56 +0300

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://youssefebeid5:Youssef43*@cluster0.lh091.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+//module.exports = { MONGOURI: 'mongodb+srv://youssefebeid5:Youssef43*@cluster0.lh091.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' }
